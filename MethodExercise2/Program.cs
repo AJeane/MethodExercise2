d6b8d6d5f776ac18c8f4a29b048fd64fac93e38e
AJeane <ajeane1924@gmail.com>
2021-06-14 23:16:36 -0500

    initial commit

---
+++ MethodExercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!")
+
+            string name = Console.ReadLine();
+
+            var result = Sum(10,
+                             20);
+
+        }
+
+        public static object Sum(int v1, int v2) => throw new NotImplementedException();
+    }
+}
